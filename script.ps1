aad3019f1124620847c726477979b08754c7b9cf
recomrad <vt@krdnet.ru>
2025-08-29 11:58:37 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,15 @@
+#Имя подключения
+$Target = "Network Interface Name"
+#Адрес днс сервера
+$DNS    = "192.168.1.1"
+# Проверяем наличие адаптера
+$adapter = Get-NetAdapter | Where-Object { $_.Name -eq $Target -and $_.Status -eq "Up" }
+#Нет такого адаптера, выход 1
+if (-not $adapter) {
+    exit 1
+}
+#Ставим днс через нетсх
+netsh interface ip set dns name="$Target" static $DNS
+#Успешно, выход 0
+exit 0
 
